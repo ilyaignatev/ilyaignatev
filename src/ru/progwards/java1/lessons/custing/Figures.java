e4c504fb11eacc2f9ea95952f11c08b9a17c35e7
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-06-30 23:47:04 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/custing/Figures.java
+++ src/ru/progwards/java1/lessons/custing/Figures.java
@@ -15,13 +15,9 @@ public class Figures {
         double p = 0.5*(a + b + c);
         return Math.sqrt(p* (p - a) * (p - b) * (p - c));
     }
-    public static double squareVsTraiange(double p){
-        return  square()/triangle();
-    }
     public static void main(String[] args) {
         System.out.println(circle(2.0));
         System.out.println(square(3.0));
         System.out.println(triangle());
-        System.out.println(square());
     }
 }
