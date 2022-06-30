5b92bad65eef8282d77526f1d3c60332f6939b93
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-06-30 23:00:34 +0300

    ilyaignatev

---
+++ src/Figures.java
@@ -0,0 +1,25 @@
+public class Figures {
+    static final double PI = 3.14;
+    public static double circle(double d){
+        return 4 * PI * (d*2);
+    }
+    public static double square(double n){
+        return n * n;
+    }
+    public static Double triangle(){
+        double a = 10.0;
+        double b = 10.0;
+        double c = 5.0;
+        double p = 0.5*(a + b + c);
+        return Math.sqrt(p* (p - a) * (p - b) * (p - c));
+    }
+    public static double squareVsTraiange(double p){
+        return  square()/triangle();
+    }
+    public static void main(String[] args) {
+        System.out.println(circle(2.0));
+        System.out.println(square(3.0));
+        System.out.println(triangle());
+        System.out.println(square());
+    }
+}
---
+++ src/ru/progwards/java1/lessons/custing/OneLine.java
@@ -0,0 +1,24 @@
+package ru.progwards.java1.lessons.custing;
+
+public class OneLine {
+    public static String strValue(String value){
+        return "Передана строка со значением " + value;
+    }
+    public static String intValue(String variable, int value) {
+        return "Значение " + variable + " равно " + value;
+    }
+    public static String square(int n){
+        return "Число " + n + " в квадрате равно " + Math.pow(n,2);
+    }
+    public static String sum(int n, int m){
+        return "Сумма " + n + " и " + m + " равна " + (n + m);
+    }
+    public static void main(String[] args) {
+        System.out.println(strValue("10"));
+        System.out.println(intValue("radius",123));
+        System.out.println(square(2));
+        System.out.println(sum(2,3));
+    }
+}
+
+
