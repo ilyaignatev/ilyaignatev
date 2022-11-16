6efe66aa26eb8717f9661c1251ab8b11f62ad01e
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-11-16 22:35:31 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/classes1/Count.java
+++ src/ru/progwards/java1/lessons/classes1/Count.java
@@ -2,33 +2,39 @@ package ru.progwards.java1.lessons.classes1;
 
 public class Count {
     int count;
-    public Count(int count){
-    this.count = count(0);
+
+    public Count(int count) {
+        count = 0;
     }
 
     private int count(int i) {
-        int count1 = 0;
-        return count1;
+        this.count = count;
     }
 
-    public int getCount(){
+    public int getCount() {
         return count;
     }
+
     public void inc() {
         count++;
     }
-    public boolean dec(){
-        if(count<= 0){
-            count--;
+
+    public boolean dec() {
+        count--;
+        if (count <= 0) {
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
+
     public static void main(String[] args) {
         Count count1 = new Count(10);
-        for(int count = 0; count <= 0; count--){
-            System.out.println("count = 0");
-            break;
+        while (true) {
+            if (count1.dec()) {
+                System.out.println("count = 0");
+                break;
+            }
         }
     }
 }
