28bcba1eddad4883917881666935b558fb0d6657
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-07 23:33:30 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/classes1/Time.java
+++ src/ru/progwards/java1/lessons/classes1/Time.java
@@ -1,6 +1,5 @@
 package ru.progwards.java1.lessons.classes1;
 
-
 public class Time {
     int hours;
     int minutes;
@@ -10,7 +9,7 @@ public class Time {
         minutes = 0;
         seconds = 0;
     }
-    public Time(int hours,int minutes,int seconds){
+    public Time(int hours,int minutes,int seconds) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
@@ -19,15 +18,12 @@ public class Time {
         return String.format("%02d:%02d:%02d", hours,minutes,seconds);
     }
     public int toSeconds(){
+
         return hours * 60 * 60 + minutes * 60 + seconds;
     }
     public int secondsBetween(Time time) {
-        Time time1 = null;
-        Time time2 = null;
-        return time2.toSeconds()-time1.toSeconds();
+        return Math.abs(this.hours * 60 * 60 + this.minutes * 60 + seconds - time.toSeconds());
     }
-
-
     public static void main(String[] args) {
     }
 }
