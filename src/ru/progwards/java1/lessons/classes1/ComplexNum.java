8ddf5d3c430b5141386c894906ea3dfb1dc99370
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-13 22:09:35 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/classes1/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes1/ComplexNum.java
@@ -13,19 +13,17 @@ package ru.progwards.java1.lessons.classes1;
         public String toString(){
             return a + "+" + b + "i";
         }
-        public ComplexNum add(ComplexNum num1,ComplexNum num2){
+        public ComplexNum add(ComplexNum num1,ComplexNum b){
             return new ComplexNum(num1.a+this.a,num1.b+this.b);
         }
 
-        public ComplexNum sub(ComplexNum num1,ComplexNum num2){
+        public ComplexNum sub(ComplexNum num1,ComplexNum b){
             return new ComplexNum(num1.a+this.a,num1.b+this.b);
         }
-        public ComplexNum mul(ComplexNum num1,ComplexNum num2){
-            int a = num1.a, b = num1.b, c = num2.a, d = num2.b;
+        public ComplexNum mul(ComplexNum num1,ComplexNum b){
             return new ComplexNum(num1.a+this.a,num1.b+this.b);
         }
-        public ComplexNum div(ComplexNum num1, ComplexNum num2){
-            int a = num1.a, b = num1.b, c = num2.a, d = num2.b;
+        public ComplexNum div(ComplexNum num1, ComplexNum numb){
             return newComplexNum(num1.a+this.a,num1.b+this.b);
         }
 
