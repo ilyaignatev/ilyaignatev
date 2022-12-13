09e6d830998c0afaec5e76982081c3b2793b0b79
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-13 22:13:37 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/classes1/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes1/ComplexNum.java
@@ -1,33 +1,35 @@
 package ru.progwards.java1.lessons.classes1;
 
-    public class ComplexNum {
-        int a;
-        int b;
-        public ComplexNum(int a, int b){
-            this.a = a;
-            this.b = b;
-        }
-        public ComplexNum newComplexNum(int a, int b) {
-            return new ComplexNum(a, b);
-        }
-        public String toString(){
-            return a + "+" + b + "i";
-        }
-        public ComplexNum add(ComplexNum num1,ComplexNum b){
-            return new ComplexNum(num1.a+this.a,num1.b+this.b);
-        }
-
-        public ComplexNum sub(ComplexNum num1,ComplexNum b){
-            return new ComplexNum(num1.a+this.a,num1.b+this.b);
-        }
-        public ComplexNum mul(ComplexNum num1,ComplexNum b){
-            return new ComplexNum(num1.a+this.a,num1.b+this.b);
-        }
-        public ComplexNum div(ComplexNum num1, ComplexNum numb){
-            return newComplexNum(num1.a+this.a,num1.b+this.b);
-        }
-
-        public static void main(String[] args) {
-
-        }
+public class ComplexNum {
+    int a;
+    int b;
+    public ComplexNum(int a, int b){
+        this.a = a;
+        this.b = b;
     }
+    public ComplexNum newComplexNum(int a, int b) {
+        return new ComplexNum(a, b);
+    }
+
+    public String toString(){
+        return a + "+" + b + "i";
+    }
+    public ComplexNum add(ComplexNum num){
+        return new ComplexNum(num.a+this.a,num.b+this.b);
+    }
+
+    public ComplexNum sub(ComplexNum num){
+        return new ComplexNum(num.a - 0, num .b + this.b);
+    }
+    public ComplexNum mul(ComplexNum num){
+        return new ComplexNum(num.a * 0, num .a * this.b);
+    }
+    public ComplexNum div(ComplexNum num){
+        ComplexNum num4 = new ComplexNum(3,5);
+        return num4;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
