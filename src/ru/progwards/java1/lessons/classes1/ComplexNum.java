64510c2c018b5087d438c7f585bb74105731b6fc
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-13 22:02:31 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/classes1/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes1/ComplexNum.java
@@ -1,37 +1,36 @@
 package ru.progwards.java1.lessons.classes1;
 
+    public class ComplexNum {
+        int a;
+        int b;
+        public ComplexNum(int a, int b){
+            this.a = a;
+            this.b = b;
+        }
+        public ru.progwards.java1.lessons.classes1.ComplexNum newComplexNum(int a, int b) {
+            return new ru.progwards.java1.lessons.classes1.ComplexNum(a, b);
+        }
+        @Override
+        public String toString(){
+            return a + "+" + b + "i";
+        }
+        public ru.progwards.java1.lessons.classes1.ComplexNum add(ru.progwards.java1.lessons.classes1.ComplexNum num1, ru.progwards.java1.lessons.classes1.ComplexNum num2){
+            return new ru.progwards.java1.lessons.classes1.ComplexNum(num1.a+num2.a,num1.b+num2.b);
+        }
 
-public class ComplexNum {
-    int a;
-    int b;
-    public ComplexNum(int a, int b){
-        this.a = a;
-        this.b = b;
-    }
-    public ComplexNum newComplexNum(int a, int b) {
-        return new ComplexNum(a, b);
-    }
+        public ru.progwards.java1.lessons.classes1.ComplexNum sub(ru.progwards.java1.lessons.classes1.ComplexNum num1, ru.progwards.java1.lessons.classes1.ComplexNum num2){
+            return new ru.progwards.java1.lessons.classes1.ComplexNum(num1.a - num2.a, num1.b - num2.b);
+        }
+        public ru.progwards.java1.lessons.classes1.ComplexNum mul(ru.progwards.java1.lessons.classes1.ComplexNum num1, ru.progwards.java1.lessons.classes1.ComplexNum num2){
+            int a = num1.a, b = num1.b, c = num2.a, d = num2.b;
+            return new ru.progwards.java1.lessons.classes1.ComplexNum(a * c - b * d , b * c + a *d);
+        }
+        public ru.progwards.java1.lessons.classes1.ComplexNum div(ru.progwards.java1.lessons.classes1.ComplexNum num1, ru.progwards.java1.lessons.classes1.ComplexNum num2){
+            int a = num1.a, b = num1.b, c = num2.a, d = num2.b;
+            return newComplexNum((a*c + b * d)/(c*c + d * d) , (b*c - a * d)/(c*c+d*d));
+        }
 
-    public String toString(){
-        return a + "+" + b + "i";
-    }
-    public ComplexNum add(ComplexNum num1, ComplexNum num2){
-        return new ComplexNum(num1.a+num2.a,num1.b+num2.b);
-    }
+        public static void main(String[] args) {
 
-    public ComplexNum sub(ComplexNum num1, ComplexNum num2){
-        return new ComplexNum(num1.a - num2.a, num1.b - num2.b);
+        }
     }
-    public ComplexNum mul(ComplexNum num1, ComplexNum num2){
-        int a = num1.a, b = num1.b, c = num2.a, d = num2.b;
-        return new ComplexNum(a * c - b * d , b * c + a *d);
-    }
-    public ComplexNum div(ComplexNum num1, ComplexNum num2){
-        int a = num1.a, b = num1.b, c = num2.a, d = num2.b;
-        return newComplexNum((a*c + b * d)/(c*c + d * d) , (b*c - a * d)/(c*c+d*d));
-    }
-
-    public static void main(String[] args) {
-
-    }
-}
