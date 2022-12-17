4b9e1c52310e7e2667f796c145637b70ddfd7fb6
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-17 22:52:52 +0300

    ilyaignatev

---
+++ src/ru/progwards/java1/lessons/useclasses/Count.java
@@ -0,0 +1,36 @@
+package ru.progwards.java1.lessons.useclasses;
+
+public class Count {
+    int count;
+    public Count(){
+        count = 0;
+    }
+    public Count(int count){
+        this.count = count;
+    }
+    public int getCount(){
+        return count;
+    }
+    public void inc(){
+        count++;
+    }
+    public boolean dec(){
+        count--;
+        if(count <= 0){
+             return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Count count1 = new Count(10);
+        while (true) {
+            if (count1.dec()) {
+                System.out.println("count равен 0");
+                break;
+            }
+        }
+    }
+}
---
+++ src/ru/progwards/java1/lessons/useclasses/Time.java
@@ -0,0 +1,30 @@
+package ru.progwards.java1.lessons.useclasses;
+
+public class Time {
+    int hours;
+    int minutes;
+    int seconds;
+    public Time(){
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    }
+    public Time(int hours,int minutes,int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+    public String toString(){
+        return String.format("%02d:%02d:%02d", hours,minutes,seconds);
+    }
+    public int toSeconds(){
+
+        return hours * 60 * 60 + minutes * 60 + seconds;
+    }
+    public int secondsBetween(Time time) {
+        return Math.abs(this.hours * 60 * 60 + this.minutes * 60 + seconds - time.toSeconds());
+    }
+    public static void main(String[] args) {
+    }
+}
+
