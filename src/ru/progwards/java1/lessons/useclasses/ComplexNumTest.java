5b64138f0a081741e33408854e5f1705eae254e5
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-18 21:53:45 +0300

    ilyaignatev

---
+++ src/ru/progwards/java1/lessons/useclasses/ComplexNumTest.java
@@ -0,0 +1,12 @@
+package ru.progwards.java1.lessons.useclasses;
+
+public class ComplexNumTest {
+    public static void main(String[] args) {
+        ComplexNum num0 = new ComplexNum(0,0);
+        ComplexNum num1 = new ComplexNum(1,1);
+        ComplexNum num2 = new ComplexNum(-2,-2);
+        ComplexNum num3 = new ComplexNum(3,-5);
+        System.out.println("сумма num0 и num1 равна " + num0.add(num1));
+        System.out.println("сумма num1 и num2 равна " + num1.add(num2));
+    }
+}
