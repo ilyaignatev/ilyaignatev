2b28d78a61331ebfe5e51219abb2b79f7d3b72a7
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-19 13:12:39 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/useclasses/CountTest.java
+++ src/ru/progwards/java1/lessons/useclasses/CountTest.java
@@ -2,8 +2,11 @@ package ru.progwards.java1.lessons.useclasses;
 
 public class CountTest{
     public static void testInc(int count) {
-        Count count1 = new Count();
-
+        for(int i = 0; i < count; i++){
+            Count count1 = new Count();
+            count1.inc();
+            System.out.print(" " + i + " ");
+        }
         }
 
     public static void testDec(int count){
