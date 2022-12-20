9da39b266ab24716b1cb3c60d91a9af2216e0f5d
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-20 21:51:31 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/useclasses/CountTest.java
+++ src/ru/progwards/java1/lessons/useclasses/CountTest.java
@@ -11,10 +11,17 @@ public class CountTest {
             System.out.println();
         System.out.println("тест inc окончен");
     }
-
     public static void testDec(int count) {
         Count count1 = new Count();
-    }
+        while(true){
+            count1.dec();
+            System.out.print(count1.getCount() + " count равен 0 ");
+            break;
+        }
+        if(count<= 0) {
+            System.out.println("тест dec окончен");
+        }
+        }
 
     public static void main(String[] args) {
         testInc(7);
