9d170513bfc50fd1c2c79d51349f6a1065622bcd
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-20 12:15:29 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/useclasses/CountTest.java
+++ src/ru/progwards/java1/lessons/useclasses/CountTest.java
@@ -1,29 +1,21 @@
 package ru.progwards.java1.lessons.useclasses;
 
-public class CountTest{
+public class CountTest {
     public static void testInc(int count) {
         Count count1 = new Count();
-        while (count1.getCount(" ")<count){
+        while (count1.getCount() < count) {
             count1.inc();
-            System.out.print(count1.getCount(" ") + " ");
+            System.out.print(count1.getCount() + " ");
         }
-        if (count>0)
-            System.out.println(" ");
+        if (count > 0)
+            System.out.println();
         System.out.println("тест inc окончен");
     }
 
-    public static void testDec(int count){
+    public static void testDec(int count) {
         Count count1 = new Count();
-        while (count1.getCount(" ")>=count){
-            count1.dec();
-            System.out.print(count1.getCount("") + "");
-            if(count<0){
-                System.out.println("count равен 0");
-                break;
-            }
-            System.out.println("тест dec окончен");
-        }
     }
+
     public static void main(String[] args) {
         testInc(7);
         testInc(0);
