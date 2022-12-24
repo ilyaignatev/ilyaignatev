de9f36a53b0c188a7d6e697187a28f825f92b614
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-24 22:00:55 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/inheritance/TestAnimal.java
+++ src/ru/progwards/java1/lessons/inheritance/TestAnimal.java
@@ -3,22 +3,23 @@ package ru.progwards.java1.lessons.inheritance;
 public class TestAnimal {
 
     public static void printAnimal(Animal animal) {
-
+        TestAnimal test = new TestAnimal();
+        test.toString();
 
     }
     public static void main(String[] args) {
         Cow cow1 = new Cow("Пеструшка");
-        System.out.println(cow1.toString());
+        System.out.println(cow1.toString() + " - " + cow1.say());
         Cow cow2 = new Cow("Рыжая");
-        System.out.println(cow2.toString());
+        System.out.println(cow2.toString() + " - " + cow1.say());
         Duck duck1 = new Duck("Даша");
-        System.out.println(duck1.toString());
+        System.out.println(duck1.toString() + " - " + duck1.say());
         Duck duck2 = new Duck("Маша");
-        System.out.println(duck2.toString());
+        System.out.println(duck2.toString() + " - " + duck1.say());
         Hamster hamster1 = new Hamster("Акакий");
-        System.out.println(hamster1.toString());
+        System.out.println(hamster1.toString() + " - " + hamster1.say());
         Hamster hamster2 = new Hamster("Поликарп");
-        System.out.println(hamster2.toString());
+        System.out.println(hamster2.toString() + " - " + hamster1.say());
     }
 }
 
