837358d549f1129c1a01e73d7434e1d2e0bb7573
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-25 22:01:26 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/inheritance/TimeZone.java
+++ src/ru/progwards/java1/lessons/inheritance/TimeZone.java
@@ -5,16 +5,13 @@ public class TimeZone {
     int minutes;
     public TimeZone(int hours){
         this.hours = hours;
-        minutes = 0;
+        this.minutes = 0;
     }
     public TimeZone(int hours, int minutes){
         this.hours = hours;
+        this.minutes = minutes;
     }
     public TimeZone getTimeZone(){
         return null;
-    }
-
-    public static void main(String[] args) {
-        Time tim1 = new Time();
     }
 }
