cfbd9bd152817bb3dcd505d980b0c46a03e4a5e7
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-26 22:10:36 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
+++ src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
@@ -1,20 +1,20 @@
 package ru.progwards.java1.lessons.inheritance;
 
 public class ZonedTime extends Time{
+TimeZone zone;
+public ZonedTime(int hours, int minutes, int second){
+    super(hours,minutes,second);
     TimeZone zone;
-    public ZonedTime(int hours, int minutes, int seconds){
-        ZonedTime zone1 = new ZonedTime(7,13,7);
-        this.zone = zone;
-    }
+}
     public ZonedTime(int hours, int minutes, int seconds, TimeZone zone){
-        ZonedTime zone2 = new ZonedTime(12,49,57);
+    this(hours,minutes,seconds);
+    this.zone =zone;
     }
     public TimeZone getTimeZone(){
-        return zone;
+    return zone;
     }
     @Override
     public int secondsBetween(Time time){
-        return Math.abs(this.hours * 60 * 60 + this.minutes * 60 + seconds - time.toSeconds());
+    return Math.abs(hours * 60 * 60 + minutes * 60 + seconds);
     }
-
 }
