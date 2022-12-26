e71ac27e5b4df59a72cb5da532e379ed41634d46
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2022-12-26 22:47:31 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
+++ src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
@@ -1,4 +1,19 @@
 package ru.progwards.java1.lessons.inheritance;
+//Реализовать класс ZonedTime - время с учетом часового пояса, как потомка класса Time
+//3.4 свойство
+//TimeZone zone - часовой пояс
+//
+//3.5 конструктор
+//public ZonedTime(int hours, int minutes, int seconds) - устанавливает значение времени из переданных параметров, и TimeZone как значение умолчанию, со сдвигом ноль от GMT
+//
+//3.6 конструктор
+//public ZonedTime(int hours, int minutes, int seconds, TimeZone zone) - устанавливает значение времени из переданных параметров, и zone
+//
+//3.4 переопределить метод
+//public TimeZone getTimeZone(), в данном классе должен возвращать zone
+//
+//3.5 переопределить метод public int secondsBetween(Time time), который возвращает количество секунд между 2-мя точками времени.(this и time) с учетом часового сдвига. Метод данного класса должен уметь принимать в качестве параметра экземпляры классов как Time, так и ZonedTime, для этого используйте метод getTimeZone() и учитывайте, что он может вернуть null.
+
 
 public class ZonedTime extends Time{
 TimeZone zone;
@@ -16,6 +31,6 @@ public ZonedTime(int hours, int minutes, int second){
     }
     @Override
     public int secondsBetween(Time time){
-    return Math.abs(getTimeZone().hours * 60 * 60 + getTimeZone().minutes * 60 + seconds);
+    return Math.abs(getTimeZone().hours * 60 * 60 + getTimeZone().minutes * 60 + seconds - time.toSeconds());
     }
 }
