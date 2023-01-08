4456bb77140b362746f14e484347ce18f67de85e
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-01-08 12:32:34 +0300

    ilyaignatev

---
+++ src/ru/progwards/java1/lessons/arrays1/Matrix.java
@@ -0,0 +1,45 @@
+package ru.progwards.java1.lessons.arrays1;
+
+public class Matrix {
+    private int[][]matrix;
+    public Matrix(int[][]matrix){
+        this.matrix = matrix;
+    }
+    public int maxInRow(int num){
+        num = 0;
+        int max_index = 0;
+        for(int i = 0; i <matrix.length; i++){
+            if(matrix[i][i] > num){
+                num = matrix[i][i];
+                max_index = 1;
+            }
+        }
+        return num;
+    }
+    public int maxInCol(int num){
+        return num;
+    }
+    public int max(){
+        return maxInRow(0);
+    }
+    public boolean isMatrix(){
+        return true;
+    }
+    public int[][] transposition(){
+        int n = 3;
+        int[][] transposition = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                transposition[i][j] = n*i + j;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = transposition[i][j];
+                transposition[i][j] = transposition[j][i];
+                transposition[j][i] = temp;
+            }
+        }
+        return null;
+    }
+}
