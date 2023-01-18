759a77cbcbdb6abd0ed21ce429e0f5f2ee86dcff
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-01-18 23:01:15 +0300

    ilyaignatev

---
+++ src/ru/progwards/java1/lessons/arrays2/IntArrays.java
@@ -0,0 +1,11 @@
+package ru.progwards.java1.lessons.arrays2;
+
+public class IntArrays {
+    public static String toString(int []a){
+        IntArrays ia = new IntArrays();
+        for(int i = 0; i < a.length; i++){
+            System.out.println(i);
+        }
+        return ia.toString(a);
+    }
+}
