d95d5d1f142fa61b5cdd22b677ae28d9a196c900
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-01-19 17:19:03 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/arrays2/IntArrays.java
+++ src/ru/progwards/java1/lessons/arrays2/IntArrays.java
@@ -11,12 +11,11 @@ public class IntArrays {
     }
 
     public static boolean equals1(int[] a1, int[] a2){
-        for (int i =0; i < a1.length; i++){
-            if(a1[i] == a2[i]){
-                return true;
-            }
+        if(a1.equals(a2)){
+            return true;
         }
-        return false;
+        else
+            return false;
     }
     public static boolean equals2(int[] a1, int[] a2){
         return Arrays.equals(a1, a2);
