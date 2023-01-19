1c39d9159956749a40ba4d80cac587c087b513cf
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-01-19 17:07:08 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/arrays2/IntArrays.java
+++ src/ru/progwards/java1/lessons/arrays2/IntArrays.java
@@ -3,16 +3,11 @@ package ru.progwards.java1.lessons.arrays2;
 import java.util.Arrays;
 
 public class IntArrays {
-    int[]a;
-    public IntArrays(int[] a) {
-        this.a = a;
-    }
-
     public static String toString(int []a){
         for(int i = 0; i < a.length; i++){
-            System.out.println(a[i]);
+            System.out.print(a[i] + " ");
         }
-        return String.valueOf(a);
+        return String.valueOf(a.length);
     }
 
     public static boolean equals1(int[] a1, int[] a2){
@@ -25,6 +20,11 @@ public class IntArrays {
     }
     public static boolean equals2(int[] a1, int[] a2){
         return Arrays.equals(a1, a2);
+    }
+
+    public static void main(String[] args) {
+        int[]ab = {34,124,5,7,2,5,7};
+        System.out.print(toString(ab) + " ");
     }
 }
 
