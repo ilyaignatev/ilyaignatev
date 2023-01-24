297a79464ce2a79e67294074e013c4c6bafc0092
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-01-24 23:30:03 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/arrays2/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -1,15 +1,16 @@
 package ru.progwards.java1.lessons.arrays2;
 
+import java.util.Arrays;
+
 public class DIntArray {
     private int[]DArray;
     public DIntArray(){
         this.DArray = DArray;
     }
     public void add(int num){
-        for(int i = 0; i < DArray.length; i++){
-            System.out.println((DArray.length -1) + num);
-            System.out.println(DArray[i] + 1);
-        }
+        int[]result = Arrays.copyOf(DArray,DArray.length +1);
+        result[result.length - 1] = num;
+        System.out.println(result);
     }
     public void atInsert(int pos, int num){
         System.out.println(pos + num);
--- src/ru/progwards/java1/lessons/arrays2/IntArrays.java
+++ src/ru/progwards/java1/lessons/arrays2/IntArrays.java
@@ -17,16 +17,20 @@ public class IntArrays {
     }
 
     public static boolean equals1(int[] a1, int[] a2){
-        Arrays.sort(a1);
-        Arrays.sort(a2);
+        int b = 0;
+        if(a1.length != a2.length){
+            return false;
+        }
         for(int i = 0; i < a1.length; i++){
-            for (int j = 0; j < a2.length; j++){
+            for (int j = 0; j < a1.length; j++){
                 if(a1[i] == a2[j]){
-                    return true;
+                    b++;
                 }
             }
+        }if(b != a1.length){
+            return false;
         }
-        return false;
+        return true;
     }
     public static boolean equals2(int[] a1, int[] a2){
         return Arrays.equals(a1, a2);
@@ -36,7 +40,10 @@ public class IntArrays {
         int[]ab = {34,124,5,7,2,5,7};
         System.out.println(toString(ab));
         int[]b1 = {1,2,3};
-        int[]b2 = {1,2,5};
+        int[]b2 = {1,3,5};
+        int[]b3 = {1,2,3};
+        int[]b4 = {1,3,2};
+        System.out.println(equals2(b3,b4));
         System.out.println(equals1(b1,b2));
     }
 }
