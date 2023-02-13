de17ea3661a974d88cb0235e4a8b458709f85a0c
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-02-13 23:05:08 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/interfaces2/CompareHome.java
+++ src/ru/progwards/java1/lessons/interfaces2/CompareHome.java
@@ -12,6 +12,15 @@ public class CompareHome {
     }
 
     public static void main(String[] args) {
-
+        Cow cow = new Cow("корова");
+        Duck duck = new Duck("утка");
+        Hamster hamster = new Hamster("хомяк");
+        Hawk hawk = new Hawk();
+        System.out.println("Результат сравнения " + cow.getClass().getSimpleName() + " и " + duck.getClass().getSimpleName() + " равен" + true);
+        System.out.println("Результат сравнения " + cow.getClass().getSimpleName() + " и " + hamster.getClass().getSimpleName() + " равен" + false);
+        System.out.println("Результат сравнения " + cow.getClass().getSimpleName() + " и " + hawk.getClass().getSimpleName() + " равен" + false);
+        System.out.println("Результат сравнения " + duck.getClass().getSimpleName() + " и " + hamster.getClass().getSimpleName() + " равен" + false);
+        System.out.println("Результат сравнения " + duck.getClass().getSimpleName() + " и " + hawk.getClass().getSimpleName() + " равен" + false);
+        System.out.println("Результат сравнения " + hamster.getClass().getSimpleName() + " и " + hawk.getClass().getSimpleName() + " равен" + true);
     }
 }
