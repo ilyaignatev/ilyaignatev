cf096b15d0355da69a486b0050287ce3f0bd042c
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-02-16 23:24:50 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/interfaces2/TestString.java
+++ src/ru/progwards/java1/lessons/interfaces2/TestString.java
@@ -2,17 +2,18 @@ package ru.progwards.java1.lessons.interfaces2;
 
 public class TestString {
     public static void print(ToString any){
-        Cow cow2 = new Cow("Пеструшка", 350);
-        System.out.println(cow2.getString());
+        System.out.println(any.getString());
     }
 
     public static void main(String[] args) {
-        Cow cow1 = new Cow("Пеструшка");
+        Cow cow1 = new Cow("Пеструшка", 350);
+        Cow cow = new Cow("Пеструшка");
         Duck duck1 = new Duck("Даша");
         Hamster hamster1 = new Hamster("Поликарп");
         Time time1 = new Time(1,12,55);
         ComplexNum complexNum = new ComplexNum(22,5);
         System.out.println(cow1);
+        System.out.println(cow);
         System.out.println(duck1);
         System.out.println(hamster1);
         System.out.println(time1);
