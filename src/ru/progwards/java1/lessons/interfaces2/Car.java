747cab6e3108898ecfc0df9a3c27ccd7cb34ab88
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-03-06 21:33:52 +0300

    ilyaignatev

---
+++ src/ru/progwards/java1/lessons/interfaces2/Car.java
@@ -0,0 +1,11 @@
+package ru.progwards.java1.lessons.interfaces2;
+
+public class Car {
+    double weight;
+    public Car(double weight){
+        this.weight = weight;
+    }
+    public double getWeight(){
+        return this.weight;
+    }
+}
