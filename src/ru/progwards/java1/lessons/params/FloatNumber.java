387d1b1919456068cc5efb5b7d3f67cd7583484d
ilyaignatev <106775397+ilyaignatev@users.noreply.github.com>
2023-03-30 22:16:43 +0300

    ilyaignatev

--- src/ru/progwards/java1/lessons/params/FloatNumber.java
+++ src/ru/progwards/java1/lessons/params/FloatNumber.java
@@ -1,35 +1,72 @@
 package ru.progwards.java1.lessons.params;
-
+import java.util.Arrays;
 
 public class FloatNumber {
-    private boolean sign;
-    private long mantissa;
-    private int exp;
-    private char[] a;
+    boolean sign;
+    long mantissa;
+    int exp;
 
-    public FloatNumber(boolean sign, long mantissa, int exp) {
+    FloatNumber(boolean sign, long mantissa, int exp){
         this.sign = sign;
         this.mantissa = mantissa;
         this.exp = exp;
     }
 
-    private FloatNumber(String num) {
-        a = num.toCharArray();
-        sign = a[0] != '-';
-        for (int i = 0; i < num.length() - 1; i++) {
-            if (a[i] == '.') {
-                exp = num.length() - i;
+    public FloatNumber(String num) {
+        char[] chars = num.toCharArray();
+        if (chars[0] != '-') { // положительное число
+            this.sign = true;
+            int dot = num.indexOf(".");
+            int ex = num.indexOf("E");
+
+            if (ex != -1) {  //есть степень e
+                char[] expArray = new char[chars.length - ex];
+                System.arraycopy(chars, ex, expArray, 0, chars.length - exp - 1);
+                char[] mantissaArray = new char[dot - ex];
+                System.arraycopy(chars, dot, mantissaArray, 0, ex - dot);
+                String expString = Arrays.toString(expArray);
+                String mantissaString = Arrays.toString(mantissaArray);
+                mantissa = Long.parseLong(mantissaString);
+                exp = Integer.parseInt(expString);
+            } else { // нет степени Е
+                char[] mantissaArray = new char[chars.length - dot];
+                System.arraycopy(chars, dot, mantissaArray, 0, chars.length - dot);
+                String mantissaString = Arrays.toString(mantissaArray);
+                mantissa = Long.parseLong(mantissaString);
             }
-            if (i >= exp) {
-                a[i] = a[i + 1];
+        } else { // отрицательное число
+            this.sign = false;
+            int dot = num.indexOf(".");
+            int ex = num.indexOf("E");
+
+            if (ex != -1) {  //есть степень e
+                char[] expArray = new char[chars.length - ex];
+                System.arraycopy(chars, ex, expArray, 0, chars.length - ex);
+                char[] mantissaArray = new char[ex - dot];
+                System.arraycopy(chars, dot, mantissaArray, 0, ex - dot);
+                String expString = Arrays.toString(expArray);
+                String mantissaString = Arrays.toString(mantissaArray);
+                mantissa = Long.parseLong(mantissaString);
+                exp = Integer.parseInt(expString);
+            } else { // Нет степени Е
+                char[] mantissaArray = new char[chars.length - dot];
+                System.arraycopy(chars, dot, mantissaArray, 0, chars.length - dot);
+                String mantissaString = Arrays.toString(mantissaArray);
+                mantissa = Long.parseLong(mantissaString);
             }
-            mantissa = Long.parseLong("" + a[i] + a[i + 1]);
         }
-        System.out.println(mantissa);
+    }
+
+    public String toString(){
+        if(!sign)
+            return "-" + mantissa + exp;
+        return null;
     }
 
     public static void main(String[] args) {
-        FloatNumber a = new FloatNumber("123");
+        String str = "-123.456";
+        FloatNumber fn3 = new FloatNumber("123.3Е3");
+        System.out.println(fn3.toString());
 
     }
 }
